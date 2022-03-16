cf07f45cb234a9cefbabd3b10539a11d1448b9da
CommonMatter <hoffmelanie@gmail.com>
2022-03-16 13:37:12 -0400

    basic style clean code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
 
-console.log( "ready!" );
-
 console.log("I'm here, I'm here!⚘!");
 
 
@@ -23,7 +21,7 @@ var names = ['Andre Medina' , 'carolina treviño' , 'Charis Poon' , 'Chirag Dav�
         .append(`<br>`);
     }
 
-var projects = ['Speculative Liberatory Learning Environment', 'Folder Poem', 'Passing Notes', 'Consensual Hacking', 'Other'];
+var projects = ['Speculative Liberatory Learning Environment', 'Folder Poetry', 'Passing Notes', 'Consensual Hacking', 'Other'];
 
     for (var value of projects) {
       $('.projects')
@@ -41,100 +39,6 @@ var days = ['Thursday', 'Friday'];
         .append(`<label for="${value}">${value}</label></div>`)
         .append(`<br>`);
     }
-
-
-
-
-
-
-// var href = window.location.href;
-// var dir = href.substring(0, href.lastIndexOf('/')) + "/";
-
-
-// (function makeCommunion(){
-//     // vary size for fun
-//     var word = "communion"
-//     var divsize = ((Math.random()*100) + 250).toFixed();
-//     var num = Math.floor(Math.random() * 1).toFixed();
-//     console.log(divsize);
-//
-//     $newdiv = $('<a href="'+dir+word+"/index.html"+'"><img src="assets/'+word+'/'+num+'.png" style="width:'+divsize+'px;"/></a>');
-//
-//     // make position sensitive to size and document's width
-//     var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
-//     var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-//
-//     $newdiv.css({
-//         'position':'absolute',
-//         'left':posx+'px',
-//         'top':posy+'px',
-//         'display':'none'
-//     }).appendTo( '.wrapper' ).fadeIn(100)
-// })();
-//
-// (function makeConsent(){
-//     // vary size for fun
-//     var word = "consent"
-//     var divsize = ((Math.random()*100) + 250).toFixed();
-//     var num = Math.floor(Math.random() * 1).toFixed();
-//     console.log(divsize);
-//
-//     $newdiv = $('<a href="/'+word+'"><img src="assets/'+word+'/'+num+'.png" style="width:'+divsize+'px;"/></a>');
-//
-//     // make position sensitive to size and document's width
-//     var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
-//     var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-//
-//     $newdiv.css({
-//         'position':'absolute',
-//         'left':posx+'px',
-//         'top':posy+'px',
-//         'display':'none'
-//     }).appendTo( '.wrapper' ).fadeIn(100)
-// })();
-//
-// (function makeRefusal(){
-//     // vary size for fun
-//     var word = "refusal"
-//     var divsize = ((Math.random()*100) + 250).toFixed();
-//     var num = Math.floor(Math.random() * 1).toFixed();
-//     console.log(divsize);
-//
-//     $newdiv = $('<a href="/'+word+'"><img src="assets/'+word+'/'+num+'.png" style="width:'+divsize+'px;"/></a>');
-//
-//     // make position sensitive to size and document's width
-//     var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
-//     var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-//
-//     $newdiv.css({
-//         'position':'absolute',
-//         'left':posx+'px',
-//         'top':posy+'px',
-//         'display':'none'
-//     }).appendTo( '.wrapper' ).fadeIn(100)
-// })();
-//
-// (function makeRenewal(){
-//     // vary size for fun
-//     var word = "renewal"
-//     var divsize = ((Math.random()*100) + 250).toFixed();
-//     var num = Math.floor(Math.random() * 1).toFixed();
-//     console.log(divsize);
-//
-//     $newdiv = $('<a href="/'+word+'"><img src="assets/'+word+'/'+num+'.png" style="width:'+divsize+'px;"/></a>');
-//
-//     // make position sensitive to size and document's width
-//     var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
-//     var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-//
-//     $newdiv.css({
-//         'position':'absolute',
-//         'left':posx+'px',
-//         'top':posy+'px',
-//         'display':'none'
-//     }).appendTo( '.wrapper' ).fadeIn(100)
-// })();
-
 
 
 });
