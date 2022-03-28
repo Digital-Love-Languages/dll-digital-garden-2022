a16db06ceee3472d8358af13bb78028a549a4373
notplants <max@mfowler.info>
2022-03-27 19:55:45 -0600

    regenerate

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 PROJECT_PATH = os.path.dirname(__file__)
 DATA_DIR = os.path.join(PROJECT_PATH, "dll_data")
+DATA_FILE = os.path.join(PROJECT_PATH, "data.json")
 
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
@@ -31,6 +32,24 @@ def main_page():
 #         'form.html',**locals())
 
 
+def regenerate_data_file():
+    files = os.listdir(DATA_DIR)
+    data = []
+    for f_name in files:
+        f_path = os.path.join(DATA_DIR, f_name)
+        with open(f_path, 'r') as f:
+            contents = json.loads(f.read())
+            data.append(contents)
+    with open(DATA_FILE, 'w') as f:
+        f.write(json.dumps(data))
+
+
+@app.route('/regenerate', methods = ['GET'])
+def regenerate():
+    regenerate_data_file()
+    return "data file was regenerated"
+
+
 @app.route('/form_post', methods = ['POST'])
 def form_post():
     if request.method == 'POST':
@@ -40,6 +59,8 @@ def form_post():
         file_path = os.path.join(DATA_DIR, filename)
         with open(file_path, 'w') as f:
             f.write(json.dumps(data))
+    # regenerate all data into one file
+    regenerate_data_file()
     return "your form submission was saved, thank you"
 
 
