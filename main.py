ba018eeb0051fc0ccce98df2e6eb8488a53906ba
notplants <max@mfowler.info>
2022-03-27 20:34:17 -0600

    Cors

--- main.py
+++ main.py
@@ -66,6 +66,13 @@ def form_post():
     regenerate_data_file()
     return "your form submission was saved, thank you"
 
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+#     return response
+
 
 if __name__ == "__main__":
     app.run()
